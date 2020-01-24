b4a52a46b52b24e9095a7653b5438354d655b779
Jordan Castillo <jordanxcallaway@gmail.com>
2020-01-23 19:24:28 -0800

    3 out of 4 tests working

--- app.js
+++ app.js
@@ -8,30 +8,35 @@ app.use(morgan('common'));
 
 app.get('/apps', (req, res) => {
   let filteredApps = [...appData];
-
   const { sort, genres } = req.query;
+  console.log('sort:', sort);
+  console.log('genres:', genres);
+  const validGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
 
-  const acceptedGenres = ['Action', 'Puzzle', 'Strategy', 'Casual', 'Arcade', 'Card'];
-
-
-  if (req.query.genres){
-    if(!acceptedGenres.includes(genres)){
-      return res.status(400).json({error: 'Genres must be one of the following: Action, Puzzle, Strategy, Casual, Arcade, Card'});
+  if(genres===''){
+    return res.status(400).json({error: 'Please enter a valid genre'});
+  }
+  //filtering by genre
+  if (genres){
+    let acceptedGenres = validGenres.toLowerCase();
+    if(!acceptedGenres.includes(genres.toLowerCase())){
+      return res.status(400).send('Genres must be one of the following: Action, Puzzle, Strategy, Casual, Arcade, Card');
     }
     
     const lowerGenres = req.query.genres.toLowerCase();
-    filteredApps = filteredApps.filter(app => app.genres.toLowerCase().includes(lowerGenres));
+    filteredApps = filteredApps.filter(() => genres.toLowerCase().includes(lowerGenres));
   }
 
+  //sort by rating and/or app
   if (sort){
-    if (req.query.sort !== 'rating' && req.query.sort !== 'app'){
-      return res.status(400).json({error: 'Sort must be set by "rating" or "app"'});
+    if (sort !== 'Rating' && sort !== 'App'){
+      return res.status(400).send('Sort must be set by "Rating" or "App"');
     }
 
     filteredApps.sort((a, b) => {
       // 1 | -1 to reverse the order || 0 to leave it where it is 
       //use bracket notation to handle dynamic data - we do not know if sort will be by title or by rank
-      return a[req.query.sort] < b[req.query.sort]? -1 : 1;
+      return a[sort] < b[sort] ? -1 : 1;
     });
   }
 
--- test/app.test.js
+++ test/app.test.js
@@ -8,8 +8,31 @@ describe('GooglePlay App', () => {
       .get('/apps')
       .expect(200)
       .expect('Content-Type', /json/)
-      // .then(res => {
-      //   expect(res.body).to.be.an('array');
-      // });
+      .then(res => {
+        expect(res.body).to.be.an('array');
+        const app = res.body[0];
+        expect(app).to.include.all.keys('App', 'Genres', 'Rating');
+      });
+  });
+
+  it('should be 400 is no genre is given', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({genres: ''})
+      .expect(400);
+  });
+
+  it('should be 400 if genres is invalid', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({ genres : 'Mystery'})
+      .expect(400, 'Genres must be one of the following: Action, Puzzle, Strategy, Casual, Arcade, Card');
+  });
+
+  it('sort should be by rating or app', () => {
+    return supertest(app)
+      .get('/apps')
+      .query({sort : 'Price'})
+      .expect(400, 'Sort must be set by "Rating" or "App"');
   });
 });
